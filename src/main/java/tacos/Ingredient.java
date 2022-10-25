d90476c79501284dd7e5983d3c5f00331c078d60
Symbat Dulatuly <synbat1999@gmail.com>
2022-10-25 15:20:08 -0700

    Initial commit

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,21 @@
+package tacos;
+
+import lombok.*;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Ingredient {
+    @Id
+    private final String id;
+    private final String name;
+    private final Type type;
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
